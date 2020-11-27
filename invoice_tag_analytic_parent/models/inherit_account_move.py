cbf02ea3494bf233e11016d3f9b44897339de86f
Real Haque <real_ariful@hotmail.com>
2020-11-27 16:26:17 +0600

    [Update] Multiple Add Tags

--- invoice_tag_analytic_parent/models/inherit_account_move.py
+++ invoice_tag_analytic_parent/models/inherit_account_move.py
@@ -7,29 +7,28 @@ class accountMoveInherit(models.Model):
 
     @api.model
     def create(self, vals):
-        for lines in vals.get('line_ids'):
-            if lines[-1].get('analytic_tag_ids'):
-                existing_tags = lines[-1].get('analytic_tag_ids')[0][-1]
-                if len(existing_tags) >0:
-                    for tags_sel in existing_tags:
-                        domain=[
-                            ('analytic_tag_ids','=',tags_sel),                             
-                            ]
-                        child_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
-                        existing_tags += child_tag_ids
+        if vals.get('line_ids'):
+            for lines in vals.get('line_ids'):
+                if lines[-1].get('analytic_tag_ids'):
+                    existing_tags = lines[-1].get('analytic_tag_ids')[0][-1]
+                    if len(existing_tags) >0:
+                        for tags_sel in existing_tags:
+                            domain=[
+                                ('analytic_tag_ids','=',tags_sel),                             
+                                ]
+                            child_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
+                            existing_tags += child_tag_ids
 
         obj = super(accountMoveInherit, self).create(vals)
         # import pdb; pdb.set_trace()
-                
-
         return obj
 
     
     def write(self, vals):
-        # import pdb; pdb.set_trace()
+        existing_tags = []
+
         if vals.get('line_ids'):
             for lines in vals.get('line_ids'):
-                # import pdb; pdb.set_trace()
                 if lines[-1] and type(lines[-1]) == dict:
                     if lines[-1].get('analytic_tag_ids'):
                         existing_tags = lines[-1].get('analytic_tag_ids')[0][-1]
@@ -40,10 +39,47 @@ class accountMoveInherit(models.Model):
                                     ]
                                 child_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
                                 existing_tags += child_tag_ids
+        
+        if vals.get('invoice_line_ids'):
+            for lines in vals.get('invoice_line_ids'):
+                if lines[-1] and type(lines[-1]) == dict:
+                    if lines[-1].get('analytic_tag_ids'):
+                        invoice_tags = lines[-1].get('analytic_tag_ids')[0][-1]
+                        if len(invoice_tags) >0:
+                            for tag in invoice_tags:
+                                domain=[
+                                    ('analytic_tag_ids','=',tag),                             
+                                    ]
+                                child_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
+                                for c_tag in child_tag_ids:
+                                    if c_tag not in existing_tags:
+                                        existing_tags += child_tag_ids
+                                if len(child_tag_ids) == 0 and tag not in existing_tags:
+                                    existing_tags += [tag]
 
 
-
+        print(existing_tags)
         obj = super(accountMoveInherit, self).write(vals)
         # if values.get('line_ids')[0][2].get('requisition_line_id'):
         #     
         return obj
+
+
+# class accountMoveLinesInherit(models.Model):
+#     _inherit = "account.move.line"
+#     @api.onchange('analytic_tag_ids')
+#     def _onchange_analytic_tag_ids(self):
+#         print(self.analytic_tag_ids)
+        
+#         existing_tags = self.analytic_tag_ids.ids
+#         for tags_sel in self.analytic_tag_ids.ids:
+#             domain=[
+#                 ('analytic_tag_ids','=',tags_sel),                             
+#                 ]
+#             child_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
+#             for child_tag in child_tag_ids:
+#                 if child_tag not in existing_tags:
+#                     existing_tags += child_tag_ids
+#         import pdb; pdb.set_trace()
+#         self.sudo().write({'analytic_tag_ids':existing_tags})
+
