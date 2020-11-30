361b995a0ec4f840ebb19661428e5ef9f4175693
Real Haque <real_ariful@hotmail.com>
2020-11-30 13:50:22 +0600

    [Update] CHild Tag Ids

--- invoice_tag_analytic_parent/models/inherit_account_move.py
+++ invoice_tag_analytic_parent/models/inherit_account_move.py
@@ -24,6 +24,7 @@ class accountMoveInherit(models.Model):
 
     
     def write(self, vals):
+        print(vals)
         existing_tags = []
 
         if vals.get('line_ids'):
@@ -41,7 +42,7 @@ class accountMoveInherit(models.Model):
                                 domain2 = [
                                     ('child_analytic_tag_ids','=',tags_sel),                             
                                     ]
-                                parent_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
+                                parent_tag_ids = self.env['account.analytic.default'].search(domain2).mapped('child_analytic_tag_ids').ids
                                 existing_tags += parent_tag_ids
         if vals.get('invoice_line_ids'):
             for lines in vals.get('invoice_line_ids'):
@@ -74,7 +75,9 @@ class accountMoveInherit(models.Model):
                                     existing_tags += [tag]
                                 if len(parent_tag_ids) == 0 and tag not in existing_tags:
                                     existing_tags += [tag]
-
+            # import pdb; pdb.set_trace()
+            # if len(vals.get('invoice_line_ids')[0][-1].get('analytic_tag_ids')[0][-1]) == 0:
+            #     existing_tags = []
         print(existing_tags)
         obj = super(accountMoveInherit, self).write(vals)
         # if values.get('line_ids')[0][2].get('requisition_line_id'):
@@ -89,19 +92,19 @@ class accountMoveLinesInherit(models.Model):
         parent_ids = []
         for tag_id in self.analytic_tag_ids[0:-1]:
             tagid= tag_id.id.origin if type(tag_id.id) != int else tag_id.id
-            domain2 = [
-                ('child_analytic_tag_ids','=',tagid),                             
+            domain = [
+                ('analytic_tag_ids','=',tagid),                             
                 ]
-            parent_tag_ids = self.env['account.analytic.default'].search(domain2).mapped('analytic_tag_ids').ids
+            parent_tag_ids = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
             parent_ids += parent_tag_ids
 
         if len(self.analytic_tag_ids) > 1:
             newtag = self.analytic_tag_ids[-1]
-            tagid= newtag.id.origin if type(tag_id.id) != int else newtag.id
+            tagid = newtag.id.origin if type(tag_id.id) != int else newtag.id
             domain = [
-                ('child_analytic_tag_ids','=',tagid),                             
+                ('analytic_tag_ids','=',tagid),                             
                 ]
-            parent_new = self.env['account.analytic.default'].search(domain).mapped('analytic_tag_ids').ids
+            parent_new = self.env['account.analytic.default'].search(domain).mapped('child_analytic_tag_ids').ids
             for item in parent_new:
                 if item in parent_ids:
                     raise UserError("You cannot select more than one child tag for same parent tag")
